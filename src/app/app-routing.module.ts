fed79d6f54e8dcee5f0010a1df1d9d5a732eb4ce
Yevgeny Levin <devlev1980@gmail.com>
2020-01-18 21:50:46 +0200

    fix ngClass

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {AddCategoryComponent} from './components/add-category/add-category.component';
 
 const routes: Routes = [
 
